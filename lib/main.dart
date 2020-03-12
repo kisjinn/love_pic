c0615f6072c86ab53d8801fee9f83e857470c236
Sakshi <sakshijinnewar@gmail.com>
2020-03-12 21:21:42 +0530

    Improving Search

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-void maun(){
-  runApp(
-     new MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: FirstPage(),
-     )
-  );
+void maun() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: FirstPage(),
+  ));
 }
 
-
-
-class FirstPage  extends StatelessWidget {
+class FirstPage extends StatelessWidget {
   var _categoryNameController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -21,21 +17,44 @@ class FirstPage  extends StatelessWidget {
         child: Center(
           child: ListView(
             children: <Widget>[
-              Padding(padding: const EdgeInsets.all(30.0), ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+              ),
               new Image.asset('images/download.png',
-              width: 200.0,
-              height: 200.0),
+                  width: 200.0, height: 200.0),
               new ListTile(
                 title: new TextFormField(
                   controller: _categoryNameController,
                   decoration: new InputDecoration(
                     labelText: 'Enter a Category',
                     hintText: 'eg: dogs,bikes,cats',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0)),
+                    contentPadding:
+                        const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
                   ),
                 ),
-              )
-
-
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+              ),
+              new ListTile(
+                title: new Material(
+                  color: Colors.lightBlue,
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(25.0),
+                  child: new MaterialButton(
+                    height: 47.5,
+                    onPressed: () {},
+                    child: Text(
+                      'Search',
+                      style: TextStyle(
+                          fontSize: 22.0, fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
